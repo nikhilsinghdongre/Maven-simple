7959c6aa5ee25fcbb71686c4c405557ac82571d0
Nikhil Dongare <46396692+nikhilsinghdongre@users.noreply.github.com>
2023-03-12 15:40:55 +0530

    Update BlogServiceTest.java

--- Tutorial/src/test/java/com/mindtree/demo/service/BlogServiceTest.java
+++ Tutorial/src/test/java/com/mindtree/demo/service/BlogServiceTest.java
@@ -1,22 +1,48 @@
 package com.mindtree.demo.service;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.mindtree.demo.entity.Blog;
+import com.mindtree.demo.repository.BlogRepo;
 @SpringBootTest
 class BlogServiceTest {
 	
+	@MockBean
+	private BlogRepo dao;
 	
+	@Autowired
+	private BlogService service;
 
 	@Test
 	void testCreateBlog() {
-		fail("Not yet implemented");
+		Blog blog  = new Blog();
+		blog.setId(100);
+		blog.setName("Tester");
+		blog.setMessage("Tesing");
+		when(dao.save(blog)).thenReturn(blog);
+		assertEquals(service.createBlog(blog), blog);
 	}
 
 	@Test
 	void testGetAllBlogs() {
-		fail("Not yet implemented");
+		Blog blog  = new Blog();
+		blog.setId(100);
+		blog.setName("Tester");
+		blog.setMessage("Tesing");
+		List<Blog> blogs = new ArrayList<>() ;
+		blogs.add(blog);
+		when(dao.findAll()).thenReturn(blogs);
+		assertEquals(service.getAllBlogs(), blogs);
 	}
 
 }
+
