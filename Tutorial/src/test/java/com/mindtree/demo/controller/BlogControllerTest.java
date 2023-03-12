bdf4c8a8f5cef923e2797c92319c7c5dd2471a25
Nikhil Dongare <46396692+nikhilsinghdongre@users.noreply.github.com>
2023-03-12 15:41:24 +0530

    Update BlogControllerTest.java

--- Tutorial/src/test/java/com/mindtree/demo/controller/BlogControllerTest.java
+++ Tutorial/src/test/java/com/mindtree/demo/controller/BlogControllerTest.java
@@ -1,19 +1,48 @@
 package com.mindtree.demo.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import com.mindtree.demo.entity.Blog;
+import com.mindtree.demo.service.BlogService;
+@SpringBootTest
 class BlogControllerTest {
+	
+	@Mock
+	private BlogService service;
 
+	@InjectMocks
+	private BlogController controller;
+	
 	@Test
 	void testCreateBlog() {
-		fail("Not yet implemented");
+		Blog blog  = new Blog();
+		blog.setId(100);
+		blog.setName("Tester");
+		blog.setMessage("Tesing");
+		when(service.createBlog(blog)).thenReturn(blog);
+		assertEquals(controller.createBlog(blog), blog);
 	}
 
 	@Test
 	void testGetAll() {
-		fail("Not yet implemented");
+		Blog blog  = new Blog();
+		blog.setId(100);
+		blog.setName("Tester");
+		blog.setMessage("Tesing");
+		List<Blog> blogs = new ArrayList<>() ;
+		blogs.add(blog);
+		when(service.getAllBlogs()).thenReturn(blogs);
+		assertEquals(controller.getAll(), blogs);		
 	}
 
 }
+
